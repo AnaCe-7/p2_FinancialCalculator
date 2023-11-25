bb348cc15af92977acf13792b2b0ba72168eb984
AnaCe-7 <ana.cecilia.reques@gmail.com>
2023-11-25 20:39:46 +0100

    Call the elements display and buttons and add an eventto show on click

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-const display = document.querySelector("display");
-const buttons = document.querySelector("btn");
+window.addEventListener('load', ()=> {
+    const display = document.querySelector(".calculator-display");
+    const calculatorButtons = document.getElementsByClassName('btn');
+
+    const calculatorButtonsArray = Array.from(calculatorButtons);
+
+    calculatorButtonsArray.forEach( (button) => {
+        button.addEventListener('click', ()=> {
+            console.log(button.innerHTML);            
+        })
+    })
+});
+
+
+
+
+
+
+
